0a753a5369ea977da8b3624b8c5d960a7cad2fc9
maverick <pelubs2956@gmail.com>
2025-05-29 08:01:13 +0100

    fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider } from './contexts/theme';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
